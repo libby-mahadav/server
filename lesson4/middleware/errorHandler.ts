07fa8e24c5fea4de1f7fab11ed88829680c2f6e7
libby-mahadav <libbymdv@gmail.com>
2025-11-19 02:00:25 +0200

    Delete lesson4 directory

--- lesson4/middleware/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import {logger} from '../UTIL/logger';
-
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  logger.error("Error:", err.message || err)
-  res.status(500).json({ error: "Internal Server Error" });
-}
